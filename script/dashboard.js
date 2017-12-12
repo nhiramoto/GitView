320c468289f716369b8e4458dd36edd266b7018a
nhtoshiaki <nh_toshi.94@hotmail.com>
2017-12-12 16:42:17 -0200

    Changing header and options menu styles.

--- script/dashboard.js
+++ script/dashboard.js
@@ -7,6 +7,17 @@ const Tree = require('./Tree');
 
 $(document).ready(() => {
     $('body').fadeIn('slow');
+    let optionActive = false;
+    $('#optionButton').click(event => {
+        if (optionActive) {
+            $(event.target).removeClass('active');
+            $('.options').removeClass('active');
+        } else {
+            $(event.target).addClass('active');
+            $('.options').addClass('active');
+        }
+        optionActive = !optionActive;
+    });
 
     let svgWidth = 500, svgHeight = 500;
     let container = d3.select('#view');
